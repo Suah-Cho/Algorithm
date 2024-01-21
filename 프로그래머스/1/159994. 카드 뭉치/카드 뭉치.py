59a9fd3aa6cc0ae6b663f7c411288e90cbecbac3
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-01-21 22:01:49 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,16 +1,10 @@
 def solution(cards1, cards2, goal):
 
     for word in goal:
-        if word == cards1[0]:
-            if len(cards1) == 1:
-                continue
-            else:
-                cards1.pop(0)
-        elif word == cards2[0]:
-            if len(cards2) == 1:
-                continue
-            else:
-                cards2.pop(0)
+        if len(cards1) > 0 and word == cards1[0]:
+            cards1.pop(0)
+        elif len(cards2) > 0 and word == cards2[0]:
+            cards2.pop(0)
         else:
             return "No"
     
