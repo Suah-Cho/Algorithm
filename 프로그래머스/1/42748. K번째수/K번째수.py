35c73a8b909b2bc6d510799d543c07dcf2569127
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-01-29 22:46:39 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 28.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        arr = array[command[0]-1:command[1]]
+        arr.sort()
+        answer.append(arr[command[2]-1])
+        
+    
+    
+    return answer
