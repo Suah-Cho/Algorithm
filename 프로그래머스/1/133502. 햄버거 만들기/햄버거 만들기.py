8c230065db3bcb7d46f8d6f527fe2faca9e413de
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-02-20 13:17:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -4,42 +4,25 @@ def solution(ingredient):
     answer = 0
     
     temp = []
-    for i in ingredient:
+    for i in range(len(ingredient)):
         temp.append(i)
         
         if temp[-4:] == [1, 2, 3, 1]:
             answer += 1
-            # temp에서 해당하는 값 제거
-            del temp[-4:] # del 함수를 사용하여 [-4:]에 해당하는 값 제거
+            
+            # temp 초기화
+            temp = temp[:-5]
+            print("temp", temp)
         
     
     return answer
 
-# 시간 초과 1
-# def solution(ingredient):
-#     answer = 0
-#     ing = ''.join(list(map(str, ingredient)))
-
-#         temp = ""
-#         for i in ing:
-#             temp += i
-#             if '1231' in temp:
-#                 answer += 1
-#                 temp = temp.replace('1231', '')
-#     return answer
-
-# 시간 초과 2
-# def solution(ingredient):
-#     answer = 0
+# 시간 초과
+# ing = ''.join(list(map(str, ingredient)))
     
-#     temp = []
-#     for i in ingredient:
-#         temp.append(i)
-        
-#         if temp[-4:] == [1, 2, 3, 1]:
+#     temp = ""
+#     for i in ing:
+#         temp += i
+#         if '1231' in temp:
 #             answer += 1
-#             # temp에서 해당하는 값 제거
-#             temp = temp[:-4]
-        
-    
-#     return answer
+#             temp = temp.replace('1231', '')
