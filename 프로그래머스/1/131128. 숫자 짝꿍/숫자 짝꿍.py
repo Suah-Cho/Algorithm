4259441af7b376d5dc15891388c01130037c6ced
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-02-23 21:29:13 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,27 +1,5 @@
 # 2월 23일 중
-from collections import Counter
 
 def solution(X, Y):
     answer = ''
-    numbers = []
-    
-    x = Counter(X)
-    y = Counter(Y)
-    
-    for numx in x:
-        if numx in y:
-            numbers.append([numx, min(x[numx], y[numx])])
-    
-    if numbers == []:
-        answer = '-1'
-    else:
-        numbers.sort(reverse=True)
-        for num in numbers:
-            for i in range(num[1]):
-                answer += num[0]
-    
-
-    if answer[0] == '0':
-        answer = '0'
-    
     return answer
