9f6296e34415cf7f3bc8379a62d028b7394eb8b0
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-02-26 16:23:36 +0900

    [level 1] Title: 둘만의 암호, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -3,16 +3,7 @@
 def solution(s, skip, index):
     answer = ''
     
-    for c in s:
-        i = ord(c)
-        j = index
-        while j > 0:
-            i += 1
-            if i > ord('z'):
-                i = ord('a')
-            if chr(i) in skip:
-                j += 1
-            j -= 1
-        answer += chr(i)
+    for i in s:
+        print(i)
     
     return answer
