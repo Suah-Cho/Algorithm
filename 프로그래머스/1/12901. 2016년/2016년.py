b4f99d3c7e5d72ca6790784cbab1a977fc426150
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-02-27 23:24:01 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -2,22 +2,49 @@
 
 def solution(a, b):
     answer = ''
-    days = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU', ]
     
+    month = 0
     d = 0
     date = 0
     
     for i in range(1, a):
         if i % 2 != 0 and i <= 7:
             d = 31
+        elif i % 2 == 0 and i <= 7:
+            d = 30
         elif i == 2:
             d = 29
-        elif i % 2 == 0 and i >7:
+        elif i % 2 == 0 and i >= 8:
             d = 31
-        else:
+        elif i % 2 != 0 and i >= 8:
             d = 30
         date += d
     
     date += b
+    return days[date % 7]
+    
+#     for i in range(1, a + 1):
+#         if day + 28 > year[i]:
+#             day = (day + 28) % year[i]
+#         else:
+#             day += 28
+#         month += 1
+    
+#     print(month, day, day % 7)
+    
+#     if b % 7 == 0:
+#         return day_list[5]
+#     elif b % 7 > 5:
+#         print("out")
+#         return day_list[(b % 7) - 5]
+#     elif b % 7 < 5:
+#         print("in")
+#         return day_list[5 - (b % 7)]
+    
+    
 
-    return days[date % 7 - 1]
+    
+    
+    
+    return answer
