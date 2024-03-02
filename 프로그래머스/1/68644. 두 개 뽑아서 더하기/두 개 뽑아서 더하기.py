352c3567983f24e1aefbb404843ad0fdafc01b76
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-03-02 11:58:32 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,13 @@
+# 3월 1일 하
+
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] not in answer:
+                answer.append(numbers[i] + numbers[j])
+    
+    answer.sort()
+    
+    return answer
