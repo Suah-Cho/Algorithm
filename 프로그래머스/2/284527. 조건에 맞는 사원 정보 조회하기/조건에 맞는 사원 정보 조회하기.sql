3595271eb28b424ebe6b38928b169e685be4b803
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-03-12 16:39:26 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -3,23 +3,27 @@
 -- HR_EMPLOYEES -> EMP_NO
 -- 점수(HR_GRADE.SCORE), 사번(HR_EMPLOYEES.EMP_NO), 성명(EMP_NAME), 직책(POSITION), 이메일(EMAIL)
 
-
-# select a.score as SCORE, e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
-# from (select emp_no, sum(score) as score
-#         from hr_grade
-#         group by emp_no) a
-#         left outer join HR_EMPLOYEES e
-#         on a.emp_no = e.emp_no
-# where SCORE = max(a.score);
-
-
-select SCORE, a.EMP_NO, EMP_NAME, POSITION, EMAIL
-from (
+-- with 사용
+with a as(
     select sum(score) as SCORE, emp_no
     from hr_grade
     group by emp_no
-    order by SCORE desc
-    limit 1
-    ) a
+)
+
+select SCORE, a.EMP_NO, EMP_NAME, POSITION, EMAIL
+from a
     inner join hr_employees e
     on a.emp_no = e.emp_no
+where SCORE = (select max(SCORE) from a)
+
+-- 내가 작성한 코드
+# select SCORE, a.EMP_NO, EMP_NAME, POSITION, EMAIL
+# from (
+#     select sum(score) as SCORE, emp_no
+#     from hr_grade
+#     group by emp_no
+#     order by SCORE desc
+#     limit 1
+#     ) a
+#     inner join hr_employees e
+#     on a.emp_no = e.emp_no
