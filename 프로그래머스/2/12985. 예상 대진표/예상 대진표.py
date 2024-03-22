4ef49694d67797557d29e1c3a7c3ff252cd659ef
Suah-Cho <sacho0929@gmail.com>
2024-03-22 23:09:42 +0900

    예상대진표 코드 수정

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,10 +1,22 @@
 # 3월 15일 중
 
-def solution(n,a,b):
-    r = 1
-    a,b = min(a,b),max(a,b)
-    while not (a%2==1 and b%2==0 and a+1==b):
-        r += 1
-        a = a//2 + a%2
-        b = b//2 + b%2
-    return r
+def solution(n, a, b):
+    answer = 1
+
+    a, b = min(a, b), max(a, b)
+
+    while not (a % 2 == 1 and b % 2 == 0 and a + 1 == b):
+        answer += 1
+        a = a // 2 + a % 2
+        b = b // 2 + b % 2
+
+    return answer
+
+# def solution(n,a,b):
+#     r = 1
+#     a,b = min(a,b),max(a,b)
+#     while not (a%2==1 and b%2==0 and a+1==b):
+#         r += 1
+#         a = a//2 + a%2
+#         b = b//2 + b%2
+#     return r
