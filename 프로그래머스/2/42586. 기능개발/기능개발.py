aa3557945f354ed82b17c8df08f87faa7f36d4bb
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-03-25 16:42:27 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,45 +1,38 @@
 # 3월 25일 상
-from queue import deque
-from math import ceil
 
 def solution(progresses, speeds):
     answer = []
     
-    long = 0
+    s = []
     
-    days = deque(ceil((100 - p) / s) for p, s in zip(progresses, speeds))
+    # del progresses[:2]
+    # del speeds[:2]
+    # print(progresses)
+    # del progresses[:1]
+    # del speeds[:1]
+    # print(progresses)
     
-    while days:
-        day = days.popleft()
-
-        if day > long:
-            answer.append(1)
-            long = day
-        else:
-            answer[-1] += 1
-    
+    while True:
+        # 하루 진도율 계산
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+            
+        
+        if progresses[0] >= 100:
+            for p in progresses:
+                if p >= 100:
+                    s.append(p)
+                else:
+                    if not s:
+                        continue
+                    else:
+                        answer.append(len(s))
+                        del progresses[:len(s)]
+                        del speeds[:len(s)]
+                        s.clear()
+                        break
+        if not progresses:
+            break
+        
     
     return answer
-
-    # s = []
-#     while True:
-#             # 하루 진도율 계산
-#             for i in range(len(progresses)):
-#                 progresses[i] += speeds[i]
-
-
-#             if progresses[0] >= 100:
-#                 for p in progresses:
-#                     if p >= 100:
-#                         s.append(p)
-#                     else:
-#                         if not s:
-#                             continue
-#                         else:
-#                             answer.append(len(s))
-#                             del progresses[:len(s)]
-#                             del speeds[:len(s)]
-#                             s.clear()
-#                             break
-#             if not progresses:
-#                 break
