bf2eae51d417745b545d2684ad19a4ac76526777
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-03-29 22:16:45 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 280.00 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,16 +1,18 @@
 # 3월 26일 중
 
 def solution(elements):
+    answer = 0
     numbers = []
-    n = len(elements)
-    elements += elements[:-1]
     
-    for i, element in enumerate(elements):
-        sum = element
-        numbers.append(sum)
-        
-        for e in elements[i + 1: i + n]:
-            sum += e
-            numbers.append(sum)
-
-    return len(list(set(numbers)))
+    print(elements[:-1])
+    
+    for i in range(1, len(elements) + 1):
+        print("i", i)
+        for j in range(len(elements)):
+            # print("j", j)
+            # if j + i >= len(elements):
+            #     print(j, sum(elements[j:j+i]), elements[j:(j + i) % len(elements) - 1])
+            # else:
+                print(j, sum(elements[j:j+i]), elements[j:j+i], j + i, (j + i) % len(elements))
+    
+    return answer
