72fa2088b123727f5e9688769eb0a7c9c141679c
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2024-10-31 23:10:34 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,10 +1,30 @@
 -- 코드를 입력하세요
-SELECT i.NAME, i.DATETIME
-FROM ANIMAL_INS i
-    LEFT OUTER JOIN ANIMAL_OUTS o
-    ON i.ANIMAL_ID = o.ANIMAL_ID
-WHERE o.ANIMAL_ID IS NULL
-ORDER BY i.DATETIME
-LIMIT 3
+
+select i.name, i.datetime
+from animal_ins i
+    left outer join animal_outs o
+    on i.animal_id = o.animal_id
+where o.datetime is NULL
+order by i.datetime asc
+limit 3;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT i.NAME, i.DATETIME
+# FROM ANIMAL_INS i
+#     LEFT OUTER JOIN ANIMAL_OUTS o
+#     ON i.ANIMAL_ID = o.ANIMAL_ID
+# WHERE o.ANIMAL_ID IS NULL
+# ORDER BY i.DATETIME
+# LIMIT 3
